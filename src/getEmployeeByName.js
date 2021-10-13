c61dcfbf3b1fe12b00027d210a94b05c8c09a7dc
Renan Souza <s.renan@outlook.com>
2021-10-13 15:39:07 -0300

    Desafio 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+// Sem parâmetros, retorna um objeto vazio
+// Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+// Quando provido o último nome do funcionário, retorna o objeto do funcionário
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName !== undefined) {
+    return data.employees.find((element) =>
+      element.firstName === employeeName || element.lastName === employeeName);
+  }
+  return {};
 }
+
+console.log(getEmployeeByName('Orloff'));
 
 module.exports = getEmployeeByName;
