03d2cabba39a162041a8b822051e48fd357b1e3e
Renan Souza <s.renan@outlook.com>
2021-10-13 16:05:40 -0300

    Desafio 4 feito junto com o Josue

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+// 1 - isManager - que será responsável por verificar se uma pessoa colaboradora é gerente ou não. O retorno dessa função deve ser um booleano: true ou false;
+// 2 - getRelatedEmployees - que utiliza a primeira função para apresentar as seguintes saídas: se for uma pessoa colaboradora gerente, deve retornar um array contendo os nomes das pessoas colaboradoras que ela é responsável; se não for uma pessoa colaboradora gerente, deverá ser lançado um erro gerado com a função construtora Error da biblioteca padrão do JavaScript com a mensagem "O id inserido não é de uma pessoa colaboradora gerente!".
+
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
+// Código feito junto com o Josué
+
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const findColaborators = data.employees.filter((colaborator) =>
+      (colaborator.managers.includes(managerId)));
+    const relatadedColaborators = findColaborators.map((related) =>
+      `${related.firstName} ${related.lastName}`);
+    return relatadedColaborators;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
