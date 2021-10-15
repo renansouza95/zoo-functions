f53db728aef833c080263caaa45c2b0202bde805
Renan Souza <s.renan@outlook.com>
2021-10-14 21:25:29 -0300

    Renan 1 x 0 Lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getEmployee = data.employees.find((employee) => employee.id === id);
+  const getID = getEmployee.responsibleFor[0];
+  const getResidents = data.species.find((animal) => animal.id === getID).residents;
+  const sortAnimalsByAge = getResidents.sort((a, b) => b.age - a.age);
+  return Object.values(sortAnimalsByAge[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,54 +1,30 @@
 const data = require('../data/zoo_data');
+
 const { hours, species } = data;
 
 function getCompleteSchedule() {
-  const tuesdayAnimals = species.filter((specie) => specie.availability.includes('Tuesday'));
-  const wednesdayAnimals = species.filter((specie) => specie.availability.includes('Wednesday'));
-  const thursdayAnimals = species.filter((specie) => specie.availability.includes('Thursday'));
-  const fridayAnimals = species.filter((specie) => specie.availability.includes('Friday'));
-  const saturdayAnimals = species.filter((specie) => specie.availability.includes('Saturday'));
-  const sundayAnimals = species.filter((specie) => specie.availability.includes('Sunday'));
-  return {
-    Tuesday: {
-      officeHour: `Open from ${hours.Tuesday.open}am until ${hours.Tuesday.close}pm`,
-      exhibition: tuesdayAnimals.map((animal) => animal.name),
-    },
-    Wednesday: {
-      officeHour: `Open from ${hours.Wednesday.open}am until ${hours.Wednesday.close}pm`,
-      exhibition: wednesdayAnimals.map((animal) => animal.name),
-    },
-    Thursday: {
-      officeHour: `Open from ${hours.Thursday.open}am until ${hours.Thursday.close}pm`,
-      exhibition: thursdayAnimals.map((animal) => animal.name),
-    },
-    Friday: {
-      officeHour: `Open from ${hours.Friday.open}am until ${hours.Friday.close}pm`,
-      exhibition: fridayAnimals.map((animal) => animal.name),
-    },
-    Saturday: {
-      officeHour: `Open from ${hours.Saturday.open}am until ${hours.Saturday.close}pm`,
-      exhibition: saturdayAnimals.map((animal) => animal.name),
-    },
-    Sunday: {
-      officeHour: `Open from ${hours.Sunday.open}am until ${hours.Sunday.close}pm`,
-      exhibition: sundayAnimals.map((animal) => animal.name),
-    },
-    Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!', }
-  };
+  const schedule = {};
+  const weekDays = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  for (let i = 0; i <= 5; i += 1) {
+    const avail = species.filter((specie) => specie.availability.includes(weekDays[i]));
+    schedule[weekDays[i]] = {
+      officeHour: `Open from ${hours[weekDays[i]].open}am until ${hours[weekDays[i]].close}pm`,
+      exhibition: avail.map((animal) => animal.name) };
+  }
+  schedule.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  return schedule;
 }
 
 function getSchedule(scheduleTarget) {
   const completeSchedule = getCompleteSchedule();
+  // console.log(completeSchedule);
   const findAnimal = species.find((specie) => specie.name === scheduleTarget);
-  const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  if (daysOfWeek.includes(scheduleTarget)) {
-    return {[scheduleTarget]: completeSchedule[scheduleTarget]};
-  } else if (findAnimal) {
-    return findAnimal.availability;
+  const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  if (weekDays.includes(scheduleTarget)) {
+    return { [scheduleTarget]: completeSchedule[scheduleTarget] };
   }
+  if (findAnimal) { return findAnimal.availability; }
   return completeSchedule;
 }
-
-console.log(getSchedule('Tuesday'));
 
 module.exports = getSchedule;
