ae19423982257bd807b6dd47e6ead04449bbde0d
Renan Souza <s.renan@outlook.com>
2021-10-19 21:38:59 -0300

    Implementa a funcao do desafio 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+// Código feito com a ajuda do Rafael Carvalho e Emerson Moreira.
+function getSpeciesByIds(ids) {
+  return ids.map((id) => species.find((specie) => id.includes(specie.id)).name);
+}
+
+function locateAnimals(ids) {
+  return ids.map((id) => species.find((specie) => id.includes(specie.id)).location);
+}
+
+function fullCoverage() {
+  const zooEmployees = [];
+  employees.forEach((employee) => {
+    zooEmployees.push({
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: getSpeciesByIds(employee.responsibleFor),
+      locations: locateAnimals(employee.responsibleFor) });
+  });
+  return zooEmployees;
+}
+
+function findEmployeeByInfo({ name, id }) {
+  return fullCoverage().find((employee) => {
+    const [firstName, lastName] = employee.fullName.split(' ');
+    if (firstName === name || lastName === name || employee.id === id) {
+      return employee;
+    }
+    return null;
+  });
+}
+
+function getEmployeesCoverage(info) {
+  if (!info) return fullCoverage();
+  if (!findEmployeeByInfo(info)) {
+    throw new Error('Informações inválidas');
+  }
+  return findEmployeeByInfo(info);
 }
 
 module.exports = getEmployeesCoverage;
